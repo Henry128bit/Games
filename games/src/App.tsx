644c9dc4105bd5885ac12d992eb846f13a5e83f0
Sergey Kamensky <henry.128bit@gmail.com>
2023-06-26 21:12:23 +0400

    Hot Fix

--- games/src/App.tsx
+++ games/src/App.tsx
@@ -1,11 +1,9 @@
 import { BsFillCalendarFill } from 'react-icons/bs';
-import Like from './components/Like/Like';
 
 function App() {
   return (
     <div>
       <BsFillCalendarFill color="blue" size="100"/>
-      <Like onClick={() => console.log('like!')}/>
     </div>
   );
 }
