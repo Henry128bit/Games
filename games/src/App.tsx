b6ad7354157b2826c46919fc1cb7052c2d5b28d2
Sergey Kamensky <henry.128bit@gmail.com>
2023-07-24 20:57:16 +0400

    Sharing state between components

--- games/src/App.tsx
+++ games/src/App.tsx
@@ -1,10 +1,16 @@
-import Example1 from "./components/Example1";
+import { useState } from "react";
+
+import NavBar from "./components/NavBar";
+import Cart from "./components/Cart";
 
 function App() {
+  const [cartItems, setCartItems] = useState(['Product1', 'Product2']);
+
   return (
     <div>
-      <h1>Hello World!</h1>
-      <Example1 />
+      <h1>Example 2</h1>
+      <NavBar cartItemsCount = {cartItems.length}></NavBar>
+      <Cart cartItems = {cartItems} onClear={() => setCartItems([])}></Cart>
     </div>
   );
 }
